77b3b661268190ea03b5d45860f8ff30a2ae9f64
Ericson Castasus <castasuse@gmail.com>
2024-11-29 18:31:43 +0800

    add vercel url

--- app/lib/utilities/ably-realtime.ts
+++ app/lib/utilities/ably-realtime.ts
@@ -1,5 +1,8 @@
 import * as Ably from "ably";
 
+export const NEXT_URL =
+  process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
+
 export const realtime = new Ably.Realtime({
-  authUrl: "http://localhost:3000/api/chat",
+  authUrl: `${NEXT_URL}/api/chat`,
 });
